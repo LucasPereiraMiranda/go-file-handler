0b7d97ee61b779caadcd8e8b9535097b87e795c8
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2023-02-14 20:33:55 -0300

    refactor: use main script to handle methods

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/LucasPereiraMiranda/go-file-handler/utils"
 )
 
@@ -8,5 +10,18 @@ func main() {
 
 	filePath := "/home/lucas/dev/personal/go-file-handler/files/data.txt"
 	utils.CreateFile(filePath)
+
+	utils.CheckFileExists(filePath)
+
+	utils.WriteFileContent(filePath, "data to add in file\n")
+	content := utils.ReadFileContent(filePath)
+
+	fmt.Println("content:", content)
+
+	fileStats := utils.GetFileStats(filePath)
+
+	fmt.Println("file name:", fileStats.Name(), "file size:", fileStats.Size())
+
+	utils.DeleteFile(filePath)
 
 }
