9edfd8b283e5d98ce97ebe1c61c688dd91ed01b5
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2023-02-14 20:26:50 -0300

    feat: add method to create new empty file

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"github.com/LucasPereiraMiranda/go-file-handler/utils"
+)
+
 func main() {
+
+	filePath := "/home/lucas/dev/personal/go-file-handler/files/data.txt"
+	utils.CreateFile(filePath)
 
 }
